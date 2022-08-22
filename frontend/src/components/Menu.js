06d4c0ca5f02ffb89a5c8f8f6c23a941d172cbd8
sasha <alexpl2003@mail.ru>
2022-08-22 22:36:40 +0300

    lesson-6

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -31,14 +31,10 @@ function Navbar({navbarItems}) {
     return (
         <nav className="navbar navbar-expand-sm navbar-light" style={{'backgroundColor': '#78d2eb'}}>
             <span className="navbar-brand" href="#">ToDo</span>
-            <div className="collapse navbar-collapse" id="navbarCollapse">
+            <div className="collapse navbar-collapse" id="navbarCollapse" style={{'position': 'absolute', 'right': '0'}}>
                 <ul className="navbar-nav mr-auto">
                   {navbarItems.map((item) => <NavbarItem name={item.name} href={item.href}/>)}
                 </ul>
-                {/*<form className="form-inline mt-2 mt-md-0">
-                    <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>*/}
             </div>
         </nav>
     )
--- to_do_list/user_app/views.py
+++ to_do_list/user_app/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework.viewsets import GenericViewSet
 from .models import User
 from .serializers import UserModelSerializer
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
 
 class UserModelViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, GenericViewSet):
